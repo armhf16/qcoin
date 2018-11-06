866bdda0376d6d0798f10a1c8d70958863021114
armhf16 <BlueWolf165@gmx.de>
2018-11-06 15:56:00 +0100

    Changed genesis block

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -15,6 +15,8 @@
 
 #include <chainparamsseeds.h>
 
+#include <arith_uint256.h>
+
 static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     CMutableTransaction txNew;
@@ -49,8 +51,8 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56";
-    const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+    const char* pszTimestamp = "Genesis Block 4.11.2018";
+    const CScript genesisOutputScript = CScript() << ParseHex("04b155f11be539a173be57a1b2770d14ff8086ebb1c7ec9b317f43fb885a7847c2bbcb54d3d590570b7295dd5d00e3d2dcb9d92c36a1b78cfcf47475ed42066a78") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
@@ -117,14 +119,14 @@ public:
         pchMessageStart[1] = 0xc0;
         pchMessageStart[2] = 0xb6;
         pchMessageStart[3] = 0xdb;
-	//nDefaultPort = 9333;
-	nDefaultPort = 4040;
+	    //nDefaultPort = 9333;
+	    nDefaultPort = 4040;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1317972665, 2084524493, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1541346164, 1572451, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2"));
-        assert(genesis.hashMerkleRoot == uint256S("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+        assert(consensus.hashGenesisBlock == uint256S("0xb38213bddabadee1ebbcf7ddeca2a59d44a329cadf1af9bef8f0893d68ef39f8"));
+        assert(genesis.hashMerkleRoot == uint256S("0xe21f31cb9b8d063bc177d3ef26394bffc1eeb326c053aa0dcbdc868d0b263a0b"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
         /*vSeeds.emplace_back("seed-a.qcoin.loshan.co.uk");
@@ -133,7 +135,7 @@ public:
         vSeeds.emplace_back("dnsseed.qcoinpool.org");
         vSeeds.emplace_back("dnsseed.koin-project.com");*/
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,58); // Changed from 48
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
         base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,50);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,176);
@@ -150,6 +152,9 @@ public:
 
         checkpointData = {
             {
+                {0, uint256S("0xb38213bddabadee1ebbcf7ddeca2a59d44a329cadf1af9bef8f0893d68ef39f8")}
+            }
+            /*{
                 {  1500, uint256S("0x841a2965955dd288cfa707a755d05a54e45f8bd476835ec9af4402a2b59a2967")},
                 {  4032, uint256S("0x9ce90e427198fc0ef05e5905ce3503725b80e26afd35a987965fd7e3d9cf0846")},
                 {  8064, uint256S("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70")},
@@ -166,16 +171,16 @@ public:
                 {456000, uint256S("0xbf34f71cc6366cd487930d06be22f897e34ca6a40501ac7d401be32456372004")},
                 {638902, uint256S("0x15238656e8ec63d28de29a8c75fcf3a5819afc953dcd9cc45cecc53baec74f38")},
                 {721000, uint256S("0x198a7b4de1df9478e2463bd99d75b714eab235a2e63e741641dc8a759a9840e5")},
-            }
+            }*/
         };
 
-        chainTxData = ChainTxData{
-            // Data as of block 59c9b9d3fec105bdc716d84caa7579503d5b05b73618d0bf2d5fa639f780a011 (height 1353397).
+        //chainTxData = ChainTxData{
+            /*// Data as of block 59c9b9d3fec105bdc716d84caa7579503d5b05b73618d0bf2d5fa639f780a011 (height 1353397).
             1516406833, // * UNIX timestamp of last known number of transactions
             19831879,  // * total number of transactions between genesis and that timestamp
                     //   (the tx=... number in the SetBestChain debug.log lines)
-            0.06     // * estimated number of transactions per second after that timestamp
-        };
+            0.06*/     // * estimated number of transactions per second after that timestamp
+        //};
     }
 };
 
@@ -225,23 +230,26 @@ public:
         pchMessageStart[3] = 0xf1;
         //nDefaultPort = 19335;
         nDefaultPort = 14040;
-	nPruneAfterHeight = 1000;
+	    nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1486949366, 293345, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1541346164, 1572451, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x4966625a4b2851d9fdee139e56211a0d88575f59ed816ff5e6a63deb4e3e29a0"));
-        assert(genesis.hashMerkleRoot == uint256S("0x97ddfbbae6be97fd6cdf3e7ca13232a3afff2353e29badfab7f73011edd4ced9"));
+
+        //printf("%s", genesis.hashMerkleRoot.ToString().c_str());
+        
+        assert(consensus.hashGenesisBlock == uint256S("0xb38213bddabadee1ebbcf7ddeca2a59d44a329cadf1af9bef8f0893d68ef39f8"));
+        assert(genesis.hashMerkleRoot == uint256S("0xe21f31cb9b8d063bc177d3ef26394bffc1eeb326c053aa0dcbdc868d0b263a0b"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("testnet-seed.qcointools.com");
+        /*vSeeds.emplace_back("testnet-seed.qcointools.com");
         vSeeds.emplace_back("seed-b.qcoin.loshan.co.uk");
-        vSeeds.emplace_back("dnsseed-testnet.thrasher.io");
+        vSeeds.emplace_back("dnsseed-testnet.thrasher.io");*/
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
-        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,58);
+        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,73); // Changed from 58
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
@@ -256,16 +264,16 @@ public:
 
         checkpointData = (CCheckpointData) {
             {
-                {2056, uint256S("17748a31ba97afdc9a4f86837a39d287e3e7c7290a08a1d816c5969c78a83289")},
+                {0, uint256S("0xb38213bddabadee1ebbcf7ddeca2a59d44a329cadf1af9bef8f0893d68ef39f8")}
             }
         };
 
-        chainTxData = ChainTxData{
+        /*chainTxData = ChainTxData{
             // Data as of block a0afbded94d4be233e191525dc2d467af5c7eab3143c852c3cd549831022aad6 (height 343833)
             1516406749,
             794057,
             0.01
-        };
+        };*/
 
     }
 };
